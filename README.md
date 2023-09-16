e1228469d0f80e3c563b1c69ac5f6707f2524c7c
NetMan <13informatyka14@gmail.com>
2023-09-16 17:54:58 +0200

    Initlal commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# cloudflare-email-rules-manager
-A simple Cloudflare e-mail routing rules manager script written in Python
+# Cloudflare e-mail rules manager
+A simple Cloudflare e-mail routing rules manager script written in Python (made it because I didn't want to visit cloudflare.com each time to add/delete a rule), it can:
+- List all rules
+- Create new rules
+- Delete existing rules
+
+## Config
+You have to prepare your:
+- Global Cloudflare API key [here](https://dash.cloudflare.com/profile/api-tokens)<br>
+- Zone ID (visit Cloudflare, choose your domain for managment, and on its overview page it'll be under the API tab/tile)
+- Your e-mail address used for Cloudflare login
+<br>
+<strong>Also make sure E-mail routing is already enabled, and the Zone ID is equivalent to the domain you want to change e-mail rules for</strong><br><br>
+Then, in the cloudflare-mail.py add your secrets, run "pip install -r requirements.txt" to download the necessary "requests" library, and run the code! (python cloudflare-mail.py)
+
+## To-Do:
+- [ ] Updating rules
+- [ ] Enabling / Disabling Catch-all
+- [ ] Better way of storing secrets
+
+## License
+This project is licensed under [The Unlicense](https://choosealicense.com/licenses/unlicense/)
---
+++ cloudflare-mail.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+
+# Configuration
+ZONE_ID = "" # Your Zone ID
+EMAIL_ADDRESS = "" # Your Cloudflare E-mail address
+GLOBAL_API_KEY = "" # Your Global API Key
+
+
+
+import requests, json, time
+
+zone_id = ZONE_ID
+header_list = {
+    'X-Auth-Email': EMAIL_ADDRESS,
+    'X-Auth-Key': GLOBAL_API_KEY,
+    'Content-Type': 'application/json',
+}
+
+print('Welcome to Cloudflare simple e-mail routing rules manager')
+print('Made by https://netman.ovh - 2023')
+
+def list_rules():
+    request = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/email/routing/rules", headers=header_list)
+    if (request.status_code == "200" or request.status_code == 200):
+        results = request.json()['result']
+        rules = []
+        for result in results:
+            if request.json()['success']:
+                if result["matchers"][0]["type"] != "all":
+                    result_type = "normal"
+                    result_get_email = f"get: {result['matchers'][0]['value']}"
+                else:
+                    result_type = "catch-all"
+                    result_get_email = "catch-all"
+                
+                if result['actions'][0]['type'] != "drop":
+                    result_action = f"{result['actions'][0]['type']}: {result['actions'][0]['value'][0]}"
+                else:
+                    result_action = "drop"
+                
+                if result['enabled']:
+                    result_enabled = "enabled"
+                else:
+                    result_enabled = "disabled"
+
+                if result['name'] == "":
+                    result_name = "Unknown"
+                else:
+                    result_name = result['name']
+
+                rules.append([
+                    result_name,
+                    result_type,
+                    result_get_email,
+                    result_action,
+                    result_enabled,
+                    result['tag'],
+                ])
+            else:
+                return ConnectionError, ConnectionError
+        format = ["Name", "Type", "Catch", "Action", "Enabled"]
+        end_result = ""
+        iterate = 0
+        for rule in rules:
+            iterate += 1
+            end_result += f"{iterate}) "
+            for i in range(5):
+                comma = ","
+                if i == 4:
+                    comma = ""
+                end_result += f"{format[i]}: {rule[i]}{comma} "
+            if iterate != len(rules):
+                end_result += "\n"
+        rule_ids = []
+        for rule_id in rules:
+            rule_ids.append(rule_id[5])
+        return end_result, rule_ids
+    else:
+        return ConnectionError, ConnectionError
+
+def programme():
+    print('What would you like to do?' + "\n"
+          + '1) List all e-mail routing rules' + "\n"
+          + '2) Create a new e-mail routing rule' + "\n"
+          + '3) Update an existing e-mail routing rule' + "\n"
+          + '4) Delete an e-mail routing rule' + "\n"
+          + '5) Exit this program')
+    option = input('Select your option: ')
+    try:
+        match int(option):
+            case 1:
+                print('Listing all rules:')
+                list_rule = list_rules()
+                if list_rule[0] != ConnectionError:
+                    print(list_rule[0])
+                else:
+                    print('Error, try again')
+                input("Press Enter to continue... ")
+                programme()
+            case 2:
+                def action():
+                    print('Choose action:' + "\n"
+                        + '1) Drop' + "\n"
+                        + '2) Send to an e-mail' + "\n"
+                        + '3) Send to a worker (does nothing for now)' + "\n"
+                        + '4) Go back to the main menu')
+                    action_option = input('Select your option: ')
+                    try:
+                        match int(action_option):
+                            case 1:
+                                drop_mail = input("Which e-mail would you like to drop mails from?\nHere: ")
+                                
+                                json_data = '''{
+                                "actions": [
+                                    {
+                                    "type": "drop"
+                                    }
+                                ],
+                                "enabled": true,
+                                "matchers": [
+                                    {
+                                    "field": "to",
+                                    "type": "literal",
+                                    "value": "%s"
+                                    }
+                                ],
+                                "name": "Drop %s",
+                                "priority": 0
+                                }''' % (drop_mail, drop_mail)
+
+                                json_data_object = json.loads(json_data)
+
+                                request = requests.post(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/email/routing/rules",
+                                                        headers=header_list, json=json_data_object)
+                                if (request.status_code == "200" or request.status_code == 200):
+                                    if request.json()['success']:
+                                        print('Done!')
+                                    else:
+                                        print('Error, try again')
+                                else:
+                                    print('Error, try again')
+                                input("Press Enter to continue... ")
+                                programme()
+                            case 2:
+                                fwd_mail = input("Which e-mail would you like to forward mails FROM?\nHere: ")
+                                dest_mail = input("Which e-mail would you like to forward mails TO?\nHere: ")
+                                json_data = '''{
+                                "actions": [
+                                    {
+                                    "type": "forward",
+                                    "value": [
+                                        "%s"
+                                    ]
+                                    }
+                                ],
+                                "enabled": true,
+                                "matchers": [
+                                    {
+                                    "field": "to",
+                                    "type": "literal",
+                                    "value": "%s"
+                                    }
+                                ],
+                                "name": "Fwd %s to %s",
+                                "priority": 0
+                                }''' % (dest_mail, fwd_mail, fwd_mail, dest_mail)
+                                json_data_object = json.loads(json_data)
+                                request = requests.post(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/email/routing/rules",
+                                                        headers=header_list, json=json_data_object)
+                                if (request.status_code == "200" or request.status_code == 200):
+                                    if request.json()['success']:
+                                        print('Done!')
+                                    else:
+                                        print('Error, try again')
+                                else:
+                                    print('Error, try again')
+                                input("Press Enter to continue... ")
+                                programme()
+                            case 3:
+                                print('3')
+                                input("Press Enter to continue... ")
+                                programme()
+                            case 4:
+                                input("Press Enter to continue... ")
+                                programme()
+                            case _:
+                                print('Incorrect option, try again')
+                                input("Press Enter to continue... ")
+                                action()
+                    except ValueError:
+                        print('Incorrect option, try again')
+                        input("Press Enter to continue... ")
+                        action()
+                action()
+            case 3:
+                print('TO-DO: Update e-mail rules')
+                input("Press Enter to continue... ")
+                programme()
+            case 4:
+                print('Which rule would you like to delete?')
+                list_rule = list_rules()
+                if list_rule[0] != ConnectionError:
+                    def rule_del():
+                        print(list_rule[0])
+                        print(f"{len(list_rule[1])+1}) Go back to the main menu")
+                        rule_del_option = input('Select your option: ')
+                        try:
+                            if (int(rule_del_option) == len(list_rule[1])+1):
+                                programme()
+                            elif (int(rule_del_option) > len(list_rule[1])+1):
+                                print("Incorrect option, try again")
+                                input("Press Enter to continue... ")
+                                rule_del()
+                            elif (int(rule_del_option) <= 0):
+                                print("Incorrect option, try again")
+                                input("Press Enter to continue... ")
+                                rule_del()
+                            elif (int(rule_del_option) > 0 and int(rule_del_option) < len(list_rule[1])+1):
+                                request = requests.delete(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/email/routing/rules/{list_rule[1][int(rule_del_option)-1]}",
+                                                        headers=header_list)
+                                if (request.status_code == "200" or request.status_code == 200):
+                                    if request.json()['success']:
+                                        print('Done!')
+                                    else:
+                                        print('Error, try again')
+                                else:
+                                    print('Error, try again')
+                                input("Press Enter to continue... ")
+                                programme()
+                            else:
+                                print("Incorrect option, try again")
+                                input("Press Enter to continue... ")
+                                rule_del()
+                        except ValueError:
+                            print('Incorrect option, try again')
+                            input("Press Enter to continue... ")
+                            rule_del()
+                    rule_del()
+                else:
+                    print('Error, try again')
+                input("Press Enter to continue..." )
+                programme()
+            case 5:
+                print('Exiting...')
+                exit()
+            case _:
+                print('Incorrect option, try again.')
+                input("Press Enter to continue... ")
+                programme()
+    except ValueError:
+        print('Incorrect option, try again')
+        input("Press Enter to continue... ")
+        programme()
+
+programme()
